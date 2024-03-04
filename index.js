d0e35e9d980ffd522f9a44ba378ab2ed649dc916
Cassidy Helms <cassidy.helms@gmail.com>
2024-03-04 16:23:32 -0500

    add middleware

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 app.use(express.json())
+
+morgan.token('data', function getData(req) {
+    return JSON.stringify(req.body)
+})
+
+app.use(morgan('tiny', {
+    skip: function(req, res) { return req.method === 'POST' }
+}))
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data', {
+    skip: function(req, res) { return req.method !== 'POST' }
+}))
 
 let persons = [
     { 
@@ -79,7 +92,6 @@ app.delete('/api/persons/:id', (request, response) => {
 })
 
 app.get('/info', (request, response) => {
-    console.log(request.headers)
     return response.send(
         `<p>Phonebook has info for ${persons.length} people</p></br>${new Date()}`
     )
